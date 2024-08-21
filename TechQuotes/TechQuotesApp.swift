a59d9ba4e849eef48a738aba1b797f03ac53009d
Sebastian Alzate <sebastiansa7@icloud.com>
2024-08-21 11:05:40 -0500

    Initial Commit

---
+++ TechQuotes/TechQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechQuotesApp.swift
+//  TechQuotes
+//
+//  Created by Sebastian Alzate on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TechQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
